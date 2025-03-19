cce9b1a9aea67aa716b0e5ab8eb295a8fe58b1a9
Rahulab14 <abr14022007@gmail.com>
2025-03-19 11:26:36 +0530

    milestone 20

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,32 @@
-const express = require('express');
-const app = express();
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config({ path: 'backend/config/.env' });
+const express = require('express')
+const ErrorHandler = require('./middleware/error')
+const app = express()
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const path = require('path');
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  }))
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use('/products', express.static(path.join(__dirname, '../products')));
+
+
+if(process.env.NODE_ENV!=="PRODUCTION"){
+    require('dotenv').config({path:"backend/config/env"})
 }
-require('dotenv').config({payh:'backend/config/.env'});
-module.exports = app;
+
+
+//routes
+const user = require('./controller/user')
+const product= require('./controller/product')
+
+ app.use('/api/user',user)
+app.use('/api/product',product)
+
+app.use(ErrorHandler)
+module.exports=app
